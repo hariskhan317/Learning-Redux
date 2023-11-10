0e6847718206a289fef10e53e4530ccaa20012e6
Haris <harisgujjar317@gmail.com>
2023-11-10 16:19:25 -0500

    Pushin Redux code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, selectCount } from './features/counterSlice';
 function App() {
+  const counter = useSelector(selectCount);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <div>
+        <div></div>
+        <button onClick={()=> dispatch(increment())}>Increment</button>
+        <button onClick={()=> dispatch(decrement())}>Decrement</button>
+        <button> logic </button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
